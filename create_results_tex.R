4a0afabfd9bd5a8e5d7f00aff8317b689c95c0f6
richelbilderbeek <richel@richelbilderbeek.nl>
2021-02-22 12:58:27 +0100

    Improve TeX table display, fix https://github.com/richelbilderbeek/bbbq_article/issues/178

--- create_results_tex.R
+++ create_results_tex.R
@@ -2,10 +2,20 @@ t <- readr::read_csv("results.csv")
 
 # Pretty print
 t$mhc_class <- as.character(as.roman(t$mhc_class))
-t$n_tmh <- paste0(round(t$f_tmh * 100), "% (", t$n_tmh, "/", t$n, ")")
+
+
+
+t$n_tmh <- paste0(format(round(t$f_tmh * 100, digits = 2), nsmall = 2), "% (", t$n_tmh, "/", t$n, ")")
 t$n <- NULL
 t$n_tmp <- NULL
 t$f_tmh <- NULL
+testthat::expect_equal(names(t)[1], "mhc_class")
+testthat::expect_equal(names(t)[2], "tool")
+testthat::expect_equal(names(t)[3], "n_tmh")
+names(t) <- c("MHC class", "Tool", "n")
+testthat::expect_equal(names(t)[1], "MHC class")
+testthat::expect_equal(names(t)[2], "Tool")
+testthat::expect_equal(names(t)[3], "n")
 t
 print(
   xtable::xtable(
@@ -22,5 +32,8 @@ print(
     label = "tab:elution",
     type = "latex"
   ),
+  include.rownames = FALSE,
   file = "results.tex"
 )
+t
+readLines("results.tex")
