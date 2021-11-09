2986f35f18fcd5fce2cec1c3c07b9b5b5f76cf3d
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-09 13:28:57 +0100

    Can 'make iedb_mhc_ligand_and_allele.csv'

--- create_iedb_mhc_ligand_and_allele.R
+++ create_iedb_mhc_ligand_and_allele.R
@@ -1,9 +1,18 @@
+max_n_queries <- Inf
+
+if (pureseqtmr::is_on_ci()) {
+  max_n_queries <- 3
+}
+
+message("max_n_queries: ", max_n_queries)
+
 output_filename <- "iedb_mhc_ligand_and_allele.csv"
 message("output_filename: ", output_filename)
 
 t <- iedbr::get_all_mhc_ligand_epitopes_and_mhc_alleles(
-  max_n_queries = Inf,
+  max_n_queries = max_n_queries,
   verbose = TRUE
 )
+t
 readr::write_csv(t, output_filename)
 testthat::expect_true(file.exists(output_filename))
